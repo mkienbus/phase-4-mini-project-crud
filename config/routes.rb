e4f280cc5801b95b7d1c7895316c85130f8a0108
Michael Kienbsuch <mkienbus@gmail.com>
2021-12-13 17:55:40 -0700

    commit tree 504db6c03f3d60dd975d5572fce5ee48f6d7aee7

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :spices, only: [:index, :update, :create, :destroy]
 end
+
+# GET /spices: return an array of all spices
+# POST /spices: create a new spice
+# PATCH /spices/:id: update an existing spice
+# DELETE /spices/:id: delete an existing spice
